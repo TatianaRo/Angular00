820b7d7de6af7119f5293f4c21e6e36d3dd7027e
Rodrigues De Oliveira <trodrigueso@indracompany.com>
2020-02-17 14:41:47 -0300

    Template - final

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShowErrosComponent } from './messages/show-erros/show-erros.component';
+import { DebugCampoComponent } from './messages/debug-campo/debug-campo.component';
+import { DebugFormComponent } from './messages/debug-form/debug-form.component';
+
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShowErrosComponent,
+    DebugCampoComponent,
+    DebugFormComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports : [
+    ShowErrosComponent,
+    DebugCampoComponent,
+    DebugFormComponent
   ]
+
 })
 export class SharedModule { }
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { NgModule } from '@angular/core';
@@ -7,22 +8,19 @@ import { GrifarParagrafoDirective } from './diretivas/grifar-paragrafo.directive
 import { DataPipe } from './pipes/data.pipe';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
-import { ShowErrosComponent } from './messages/show-erros/show-erros.component';
-import { DebugCampoComponent } from './messages/debug-campo/debug-campo.component';
-import { DebugFormComponent } from './messages/debug-form/debug-form.component';
-
 
 
 @NgModule({
   declarations: [UsuarioFormComponent, 
     GrifarParagrafoDirective, 
-    DataPipe, TemplateFormComponent, DataFormComponent, ShowErrosComponent, DebugCampoComponent, DebugFormComponent
+    DataPipe, TemplateFormComponent, DataFormComponent
   ],
   imports: [
     CommonModule,
     UsuarioRoutingModule,
-    FormsModule
-
+    FormsModule,
+    SharedModule
+    
   ],
     exports: [
       UsuarioFormComponent,
